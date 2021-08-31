e370c50925a90b4950ea6c4238eb24440e35b819
KD <kdpuvvadi@gmail.com>
2021-08-31 15:09:42 +0530

    read vars

--- ddns.ps1
+++ ddns.ps1
@@ -1,5 +1,12 @@
 Write-Host "DDNS for Cloudflare on Windows 10"
 
+# Get Public IP with ifconfig
 $getIP = Invoke-RestMethod -Method Get -Uri  https://ifconfig.me/all.json
 
-Write-Host "Public IP Address of your machine is "$getIP.ip_addr
+#Printing the Public IP
+Write-Host "Public IP Address of your machine is"$getIP.ip_addr
+
+#Read variables from vars.ini
+$actVars = Get-Content -Path 'vars.ini' | ConvertFrom-StringData
+
+Write-Host $actVars.apikey
