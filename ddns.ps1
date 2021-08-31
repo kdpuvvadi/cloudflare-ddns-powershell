4c1e687b0c36d409b232f0afbc899914c4de0e3e
KD <kd@puvvadi.me>
2021-08-31 21:02:00 +0530

    added create method to fix  #1

--- ddns.ps1
+++ ddns.ps1
@@ -22,11 +22,6 @@ $getUri = "https://api.cloudflare.com/client/v4/zones/$($actVars.zoneID)/dns_rec
 #record ID with get request
 $getRecord = Invoke-RestMethod -Method Get -Headers $apiHeader -Uri $getUri -PassThru -OutFile .\log.txt
 
-
-
-
-$updateUri = "https://api.cloudflare.com/client/v4/zones/$($actVars.zoneID)/dns_records/$($getRecord.result.id)"
-
 $updateBody = @{
         "type"= "$($actVars.recordType)"
         "name"= "$($actVars.record)"
@@ -38,9 +33,25 @@ $updateBody = @{
 $jsonBody = $updateBody | ConvertTo-Json
 
 if ($null -eq $($getRecord.result)) {
-        Write-Host "$($actVars.record) does not exist on the cloudflare "
+        Write-Host "$($actVars.record) does not exist on the cloudflare"
+
+        $createUri = "https://api.cloudflare.com/client/v4/zones/$($actVars.zoneID)/dns_records/"
+
+        $createRecord = Invoke-RestMethod -Method POST -Headers $apiHeader -Uri $createUri -Body $jsonBody
+
+        if ( $createRecord.success -eq $true ) {
+                Write-Host "Success fully updated DNS record for $($actVars.record) to $($getIP.ip_addr)"
+        }
+        else {
+                Write-Host "An error occured"
+                Write-Host "$($createRecord.errors)"
+                Write-Host "$($createRecord.messages)"
+        }
+
 }
 else {
+        $updateUri = "https://api.cloudflare.com/client/v4/zones/$($actVars.zoneID)/dns_records/$($getRecord.result.id)"
+
         $updateRecord = Invoke-RestMethod -Method PUT -Headers $apiHeader -Uri $updateUri -Body $jsonBody
 
         if ( $updateRecord.success -eq $true ) {
