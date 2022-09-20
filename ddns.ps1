beb187e9a7489ac7576f70d21030e7ab4b3aa69d
KD Puvvadi <kdpuvvadi@gmail.com>
2022-09-20 11:41:08 +0530

    legacy api key to api token

--- ddns.ps1
+++ ddns.ps1
@@ -9,24 +9,29 @@ $PublicIP = $getIP.ip_addr
 
 #Read variables from ini
 $actVars = Get-Content -Path $varfile | ConvertFrom-StringData
+$TOKEN =  $actVars.apiToken
+$domain = $actVars.domain
+$subdomain = $actVars.subdomain
+$record= "$subdomain.$domain"
 
-#API auth headers
+# API auth headers with apitoken
 $apiHeader = @{
-	"X-Auth-Email" = $actVars.email
-	"X-Auth-Key"   = $actVars.apikey
+	Authorization = "Bearer $TOKEN"
 	"Content-Type" = "application/json"
 }
 
+# Get Zone id
+$zoneID=(Invoke-RestMethod -Method Get -Headers $apiHeader -Uri  https://api.cloudflare.com/client/v4/zones?name=$($actVars.domain)).result.Id
+
 #api url
-$getUri = "https://api.cloudflare.com/client/v4/zones/$($actVars.zoneID)/dns_records?type=$($actVars.recordType)&name=$($actVars.record)"
+$getUri = "https://api.cloudflare.com/client/v4/zones/$zoneID/dns_records?type=A&name=$record"
 
 #record ID with get request
 $getRecord = Invoke-RestMethod -Method Get -Headers $apiHeader -Uri $getUri -PassThru -OutFile .\log.txt
 
-
 $updateBody = @{
-	"type"    = "$($actVars.recordType)"
-	"name"    = "$($actVars.record)"
+	"type"    = "A"
+	"name"    = "$record"
 	"content" = "$($PublicIP)"
 	"ttl"     = 1
 	"proxied" = $true
@@ -38,7 +43,7 @@ $jsonBody = $updateBody | ConvertTo-Json
 #check command status
 function resultDNS ($checkRecord, $updateType) { 
 	if ( $checkRecord -eq $true ) {
-		Write-Host "Successfully $updateType DNS record for $($actVars.record) to $($PublicIP)"
+		Write-Host "Successfully $updateType DNS record for $record to $($PublicIP)"
 	}
 	else {
 		Write-Host "An error occured"		
@@ -47,18 +52,18 @@ function resultDNS ($checkRecord, $updateType) {
 
 # skip if the recrod is upto date
 if ($PublicIP -eq $getRecord.result.content ) {
-	Write-Host "$($actVars.record) is already upto date."
+	Write-Host "$record is already upto date."
 }
 # Create new record if does not exist
 elseif ($null -eq $($getRecord.result)) {
-	Write-Host "$($actVars.record) does not exist on the cloudflare"
-	$createUri = "https://api.cloudflare.com/client/v4/zones/$($actVars.zoneID)/dns_records/"
+	Write-Host "$record does not exist on the cloudflare"
+	$createUri = "https://api.cloudflare.com/client/v4/zones/$zoneID/dns_records/"
 	$createRecord = Invoke-RestMethod -Method POST -Headers $apiHeader -Uri $createUri -Body $jsonBody
 	resultDNS -checkRecord $($createRecord.success) -updateType created
 }
 # Update if the record exist but different content
 else {
-	$updateUri = "https://api.cloudflare.com/client/v4/zones/$($actVars.zoneID)/dns_records/$($getRecord.result.id)"
+	$updateUri = "https://api.cloudflare.com/client/v4/zones/$zoneID/dns_records/$($getRecord.result.id)"
 	$updateRecord = Invoke-RestMethod -Method PUT -Headers $apiHeader -Uri $updateUri -Body $jsonBody
 	resultDNS -checkRecord $($updateRecord.success) -updateType updated
 }
